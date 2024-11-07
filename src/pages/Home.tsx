394833ae96b1f353f11d69497c573ec4dd56e366
Renatuscape <silarcta@gmail.com>
2024-11-07 14:18:29 +0100

    Moves home page content to separate component
    Home page content was previously displayed directly from app.tsx. Also removes react logo, which is now displayed in header

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+export function Home() {
+    return (
+        <div className='home'>
+            <div className="card">
+                <p>
+                    Created in Vite + React and TypeScript.
+                </p>
+            </div>
+        </div>
+    )
+}
