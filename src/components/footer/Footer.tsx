dd97e826b28050ecea5dd8ab074805d568193635
Renatuscape <silarcta@gmail.com>
2024-11-07 14:16:19 +0100

    Adds footer component
    For displaying information at the bottom of the page

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export function Footer() {
+    return (
+        <div className={styles.footer}>
+            <p className="read-the-docs">
+                This page is under construction
+            </p>
+        </div>
+    )
+}
