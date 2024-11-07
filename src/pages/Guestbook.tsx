4deb5011623230696ac215dfc59385b85abd4b04
Renatuscape <silarcta@gmail.com>
2024-11-07 14:19:13 +0100

    Adds guest book component
    Page for displaying and entering new messages, once the website is connected to MongoDB

---
+++ src/pages/Guestbook.tsx
@@ -0,0 +1,15 @@
+import '../App.css'
+
+export function Guestbook() {
+    return (
+        <div className='guestbook'>
+            <h2>Guest Book</h2>
+            <div className="card">
+                <p>
+                    Welcome to the guest book!
+                </p>
+                <p>In the future, this page will display comments from guests.</p>
+            </div>
+        </div>
+    )
+}
