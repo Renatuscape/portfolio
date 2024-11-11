c3a83d4925db9b52892a392195a136e9110df8ed
Renatuscape <silarcta@gmail.com>
2024-11-11 15:07:43 +0100

    Prints out repo information with styling
    Repositories are now stored with more information and are styled to be displayed nicely

--- src/components/repocollection/RepoCollection.tsx
+++ src/components/repocollection/RepoCollection.tsx
@@ -1,11 +1,15 @@
 import { Octokit } from "octokit";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import styles from "./RepoCollection.module.css";
 
 type RepoData = {
     id: number;
     name: string;
     description: string | null;
+    html_url: string;
+    language: string;
+    languages_url: string;
 }
 
 export function RepoCollection() {
@@ -45,9 +49,27 @@ export function RepoCollection() {
         fetchUserData();
     }, [])
 
+    const repoList = repos.map((repo) =>
+        <div key={repo.id} className={styles.repoItem}>
+            <h1>{NameFormatter(repo.name)}</h1>
+            <p>{repo.description ?  repo.description : "No description"}</p>
+            <div className={styles.repoFooter}><span id={styles.langTag}>{repo.language}</span><a href={repo.html_url}>Visit</a></div>
+        </div>
+    );
+
+    // Turns all dashes into spaces and converts camelcase to spaces
+    function NameFormatter(name: string){
+    let formattedName = name.replace(/-/g, ' ');
+    formattedName = formattedName.replace(/([a-z])([A-Z])/g, '$1 $2');
+    formattedName = formattedName.replace(/\b\w/g, char => char.toUpperCase());
+
+    return formattedName;
+    }
+
     return <>
-        <p>
-            {loading ? <>Loading...</> : repos.length > 0 ? <>Done loading!</> : "Unable to find any repositories"}
-        </p>
+        {loading ? <>Loading...</> : repos.length > 0 ?
+            <div className={styles.repoCollection}>
+                {repoList}
+            </div> : "Could not load repositories"}
     </>
 }
