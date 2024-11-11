bf47c803d7d3e2f02cb4bcedbf3576b00364504c
Renatuscape <silarcta@gmail.com>
2024-11-11 12:52:58 +0100

    Replaces placeholder text with userinfo and repo collection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,25 @@
 import { Card } from '../components/card/Card'
 import { Page } from '../components/page/Page'
+import { RepoCollection } from '../components/repocollection/RepoCollection';
+import { UserInfo } from '../components/userinfo/UserInfo';
 import styles from './Pages.module.css';
 
+
 export function Home() {
+
     return (
         <Page>
             <div className={styles.homeGrid}>
                 <Card>
-                    <p>
-                        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
-                    </p>
-                </Card>
-                <Card>
+                    <UserInfo />
                     <p>
                         Created in Vite + React and TypeScript.
                     </p>
                 </Card>
+                <Card>
+                    <RepoCollection />
+                </Card>
             </div>
-        </Page>
+        </Page >
     )
 }
