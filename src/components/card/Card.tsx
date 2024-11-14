b0ffd1b47556c1d74405fd5f5dc4e3776e8ec7dd
Renatuscape <silarcta@gmail.com>
2024-11-14 09:32:00 +0100

    Adds option for custom inline styling

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,13 +1,14 @@
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 import styles from "./Card.module.css";
 
 type CardProps = {
+    customStyle?: CSSProperties;
     children: ReactNode;
 }
 
-export function Card({children}: CardProps) {
+export function Card({children, customStyle}: CardProps) {
     return (
-        <div className={styles.card}>
+        <div className={styles.card} style={customStyle}>
             {children}
         </div>
     )
