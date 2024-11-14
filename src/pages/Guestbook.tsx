c6f52e8d6e88d7a26c4174712c5db7afe441fcf4
Renatuscape <silarcta@gmail.com>
2024-11-14 15:48:59 +0100

    Improves guest book palceholder text

--- src/pages/Guestbook.tsx
+++ src/pages/Guestbook.tsx
@@ -1,14 +1,20 @@
 import { Card } from '../components/card/Card'
 import { Page } from '../components/page/Page'
+import styles from './Pages.module.css';
 
 export function Guestbook() {
     return (
         <Page>
             <Card>
-                <p>
-                    Welcome to the guest book!
-                </p>
-                <p>In the future, this page will display comments from guests.</p>
+                <div className={styles.guestBook}>
+                    <h1>Guest Book</h1>
+                    <p>
+                        Welcome!
+                    </p>
+                    <br/>
+                    <p>In the future, this page will display comments from guests.</p>
+                    <br/>
+                </div>
             </Card>
         </Page>
     )
