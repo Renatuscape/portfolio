61d099e7277cb3c254511b7f9c38a725c877151f
Renatuscape <silarcta@gmail.com>
2024-11-14 10:13:24 +0100

    Adds comments and adjusts border radius on info column

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+// The default page displayed at the root address
+
 import { Card } from '../components/card/Card'
 import { Page } from '../components/page/Page'
 import { RepoCollection } from '../components/repocollection/RepoCollection';
@@ -6,14 +8,15 @@ import styles from './Pages.module.css';
 
 
 export function Home() {
-
+    
+    // Custom card in-line styling keeps this one corner of the UserInfo Card from extending past the  icon
     return (
         <Page>
             <div className={styles.homeGrid}>
                 <div className={styles.infoColumn}>
-                <Card  customStyle={{borderTopLeftRadius: '25%'}}>
-                    <UserInfo />
-                </Card>
+                    <Card customStyle={{ borderTopLeftRadius: '85px' }}>
+                        <UserInfo />
+                    </Card>
                 </div>
                 <Card>
                     <RepoCollection />
