ca8c097e8ad386f249b7f8d72b48835e3c1306f3
Renatuscape <silarcta@gmail.com>
2024-11-18 13:04:55 +0100

    Changes links to open in new tab

--- src/components/useractivity/UserActivity.tsx
+++ src/components/useractivity/UserActivity.tsx
@@ -6,8 +6,8 @@ export function UserActivity() {
             <h1>Current Projects</h1>
             <ul>
                 <li>Building this portfolio in TypeScript/React</li>
-                <li>Polishing my JavaScript skills with <a href='https://www.theodinproject.com/'>The Odin Project</a></li>
-                <li>Working on my Unity/C# game. <a href='https://renatuscape.itch.io/a-stranger-comes-home'>Try it for free!</a></li>
+                <li>Polishing my JavaScript skills with <a target='_blank' href='https://www.theodinproject.com/'>The Odin Project</a></li>
+                <li>Working on my Unity/C# game. <a target='_blank' href='https://renatuscape.itch.io/a-stranger-comes-home'>Try it for free!</a></li>
             </ul>
         </div>
     </>
